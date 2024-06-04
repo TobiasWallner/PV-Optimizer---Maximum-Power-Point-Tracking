471866af06aa8477603bd63fe4accf3a7d2e5b79
Tobias Wallner <tobias.wallner1@gmx.net>
2024-06-04 19:53:27 +0200

    class now accepts a type parameter + added the functions sum() that returns the current sum + added a function average() that returns the current average

--- Libraries/MovingAverage.hpp
+++ Libraries/MovingAverage.hpp
@@ -6,22 +6,24 @@
  *      Author: Tobias Wallner
  */
 
-template<size_t N>
+template<class T, size_t N>
 class MovingAverage{
-	fix32<16> buffer[N]{};
-	fix32<16> sum = 0;
-	size_t index = 0;
+	T _buffer[N]{};
+	T _sum = T(0);
+	size_t _index = 0;
 
 public:
 
-	inline MovingAverage(){for(fix32<16>& elem : buffer) elem = fix32<16>::reinterpret(0);}
+	inline MovingAverage(){for(T& elem : this->_buffer) elem = T(0);}
 
-	inline fix32<16> input(fix32<16> value){
-		static const fix32<16> factor = 1 / fix32<16>(N);
-		sum += (value - buffer[index]);
-		buffer[index] = value;
-		index = (index < (N-1)) ? index+1 : 0;
-		return sum * factor;
+	inline T input(T value){
+		this->_sum += (value - this->_buffer[this->_index]);
+		this->_buffer[this->_index] = value;
+		this->_index = (this->_index < (N-1)) ? this->_index+1 : 0;
+		return this->_sum / N;
 	}
+
+	inline T sum() const {return this->_sum;}
+	inline T average() const {return this->_sum/N;}
 
 };
